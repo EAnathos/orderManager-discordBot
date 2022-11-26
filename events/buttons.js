78089ec3bb8c434085740784d850dd43143dc6db
EAnathos <eanathos@gmail.com>
2022-11-26 16:02:27 +0100

    add the last button utilities

--- events/buttons.js
+++ events/buttons.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { ActionRowBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 const { CREATED_CATEGORY, IN_PROGRESS_CATEGORY, BILLING_CATEGORY, FINISH_CATEGORY } = require('../config.json');
 
 module.exports = {
@@ -22,7 +22,59 @@ module.exports = {
                 interaction.channel.delete();
                 interaction.reply({ content: '✅Ticket supprimé', ephemeral: true});
             } else if (interaction.customId === 'modify_Id') {
-                interaction.reply({ content: '🚧Cette fonctionnalité n\'est pas encore disponible', ephemeral: true});
+                const modal = new ModalBuilder()
+                    .setCustomId('myModal2')
+                    .setTitle(`Création d'une commande`);
+        
+                // Create the text input components
+                const clientName = new TextInputBuilder()
+                    .setCustomId('clientOrder_Id')
+                    // The label is the prompt the user sees for this input
+                    .setLabel("Client de la commande")
+                    // set a placeholder string to prompt the user
+                    .setPlaceholder(`Insérer le pseudo de votre client`)
+                    // Short means only a single line of text, Paragraph means a multi-line text entry
+                    .setStyle(TextInputStyle.Short)
+                    // require a value in this input field
+                    .setRequired(true);
+
+                const serverName = new TextInputBuilder()
+                    .setCustomId('serverName_Id')
+                    .setLabel("Nom du serveur de la commande")
+                    .setPlaceholder(`Insérer le nom du serveur`)
+                    .setStyle(TextInputStyle.Short)
+                    .setRequired(true);
+
+                const pluginName = new TextInputBuilder()
+                    .setCustomId('pluginName_Id')
+                    .setLabel("Nom du plugin de la commande")
+                    .setPlaceholder(`Insérer le nom du plugin`)
+                    .setStyle(TextInputStyle.Short)
+                    .setRequired(true);
+
+                const orderPrice = new TextInputBuilder()
+                    .setCustomId('orderPrice_Id')
+                    .setLabel("Prix de la commande")
+                    .setPlaceholder(`Insérer le prix de votre commande`)
+                    .setStyle(TextInputStyle.Short)
+                    .setRequired(true);
+
+                const deadlineDate = new TextInputBuilder()
+                    .setCustomId('deadlineDate_Id')
+                    .setLabel("deadline de la commande")
+                    .setPlaceholder(`Insérer la deadline de votre commande`)
+                    .setStyle(TextInputStyle.Short)
+                    .setRequired(true);
+                
+                const firstActionRow = new ActionRowBuilder().addComponents(clientName);
+                const secondActionRow = new ActionRowBuilder().addComponents(serverName);
+                const thirdActionRow = new ActionRowBuilder().addComponents(pluginName);
+                const fourthActionRow = new ActionRowBuilder().addComponents(orderPrice);
+                const fifthActionRow = new ActionRowBuilder().addComponents(deadlineDate);;
+
+                modal.addComponents(firstActionRow, secondActionRow, thirdActionRow, fourthActionRow, fifthActionRow);
+
+                await interaction.showModal(modal);    
             } else if (interaction.customId === 'cdc_Id') {
                 const modal = new ModalBuilder()
                     .setCustomId('cdcModal')
@@ -69,7 +121,26 @@ module.exports = {
 
                 lastMessage.edit({ embeds: [lastMessage.embeds[0]], components: [rows] });
                 interaction.reply({ content: `✅ Votre cahier des charges a bien été enregistré : ${interaction.fields.getTextInputValue('cdcLink_Id')}`, ephemeral: true });
-            }
+            } else if (interaction.customId === 'myModal2') {
+                const messages = await interaction.channel.messages.fetch();
+                const lastMessage = messages.first();
+
+                const embed = new EmbedBuilder()
+                    .setTitle(`Commande demandée par ${interaction.fields.getTextInputValue('clientOrder_Id')}`)
+                    .setDescription(
+                        `⚜️ **Nom du serveur :** ${interaction.fields.getTextInputValue('serverName_Id')}
+                        \n🎫 **Nom du plugin :** ${interaction.fields.getTextInputValue('pluginName_Id')}
+                        \n💵 **Prix de la commande :** ${interaction.fields.getTextInputValue('orderPrice_Id')} €
+                        \n⌛ **Deadline de la commande :** ${interaction.fields.getTextInputValue('deadlineDate_Id')}
+                        \n🗄️ **Status de la commande :** À réaliser`
+                    )
+                    .setColor('DarkBlue')
+                    .setFooter({ text: `Bot créé par @Anathos#7090`, iconURL: "https://avatars.githubusercontent.com/u/83123402?v=4" })
+                    .setTimestamp()
+
+                lastMessage.edit({ embeds: [embed] });
+                interaction.reply({ content: `✅ Votre commande a bien été modifiée`, ephemeral: true });
+            }   
         }
     }
 }
